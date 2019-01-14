eac42640f2ac66969f86c8cd93532b57e858555b
JeffRisberg <jrisberg@aol.com>
2019-01-13 18:47:51 -0800

    lambda example

--- src/main/java/com/incra/Main.java
+++ src/main/java/com/incra/Main.java
@@ -3,25 +3,38 @@ package com.incra;
 import java.util.ArrayList;
 import java.util.List;
 
+interface MyGreeting {
+  String processName(String str);
+}
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
 
-      List<String> stringCollection = new ArrayList<>();
-      stringCollection.add("ddd2");
-      stringCollection.add("aaa2");
-      stringCollection.add("bbb1");
-      stringCollection.add("aaa1");
-      stringCollection.add("bbb3");
-      stringCollection.add("ccc");
-      stringCollection.add("bbb2");
-      stringCollection.add("ddd1");
+    MyGreeting morningGreeting = (str) -> "Good Morning " + str + "!";
+    MyGreeting eveningGreeting = (str) -> "Good Evening " + str + "!";
 
-      stringCollection
-        .stream()
-        .filter((s) -> s.startsWith("a"))
-        .forEach(System.out::println);
+    // Output: Good Morning Luis!
+    System.out.println(morningGreeting.processName("Luis"));
 
-    }
+    // Output: Good Evening Jessica!
+    System.out.println(eveningGreeting.processName("Jessica"));
+
+    List<String> stringCollection = new ArrayList<>();
+    stringCollection.add("ddd2");
+    stringCollection.add("aaa2");
+    stringCollection.add("bbb1");
+    stringCollection.add("aaa1");
+    stringCollection.add("bbb3");
+    stringCollection.add("ccc");
+    stringCollection.add("bbb2");
+    stringCollection.add("ddd1");
+
+    stringCollection
+      .stream()
+      .filter((s) -> s.startsWith("a"))
+      .forEach(System.out::println);
+
+  }
 }
